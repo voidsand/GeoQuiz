3a51c134f597443d46f76337f9fdb631f95f0b4f
voidsand <voidsand@163.com>
2017-11-07 23:15:09 +0800

    Initial commit

---
+++ app/src/main/java/com/voidsand/test/geoquiz/Question.java
@@ -0,0 +1,30 @@
+package com.voidsand.test.geoquiz;
+
+/**
+ * Created by Yu on 2017/11/7.
+ */
+
+public class Question {
+    private int mTextResId;
+    private boolean mAnswerTrue;
+    public Question(int textResId, boolean answerTrue) {
+        mTextResId = textResId;
+        mAnswerTrue = answerTrue;
+    }
+
+    public int getTextResId() {
+        return mTextResId;
+    }
+
+    public void setTextResId(int textResId) {
+        mTextResId = textResId;
+    }
+
+    public boolean isAnswerTrue() {
+        return mAnswerTrue;
+    }
+
+    public void setAnswerTrue(boolean answerTrue) {
+        mAnswerTrue = answerTrue;
+    }
+}
